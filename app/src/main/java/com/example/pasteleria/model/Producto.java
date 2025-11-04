36e87b8c49525b1d60abe5690e93b7edb86fe4b8
Maria0412 <kothagatica@gmail.com>
2025-11-04 14:38:40 -0300

    Update Producto.java
    Se modifica la clase producto, agregando los atributos de producto

--- app/src/main/java/com/example/pasteleria/model/Producto.java
+++ app/src/main/java/com/example/pasteleria/model/Producto.java
@@ -1,4 +1,9 @@
 package com.example.pasteleria.model;
 
-public class Producto {
+data class Producto {
+    val nombre: String,
+    val precio: Int,
+    var cantidad: Int = 1,
+    @DrawableRes val imagen: Int
+  
 }
