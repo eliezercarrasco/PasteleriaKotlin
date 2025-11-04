0f323df3cba64edc12b5877b0d73c4e01c6d202c
Maria0412 <kothagatica@gmail.com>
2025-11-04 14:41:48 -0300

    Update Usuario.java
    Se modifica clase usuario, agregando sus atributos

--- app/src/main/java/com/example/pasteleria/model/Usuario.java
+++ app/src/main/java/com/example/pasteleria/model/Usuario.java
@@ -1,4 +1,14 @@
 package com.example.pasteleria.model;
 
-public class Usuario {
-}
+data class Usuario {
+    val nombre: String,
+    val correo: String,
+    val password: String,
+    val telefono: String?,
+    val direccion: String,
+    val edad: Int,
+    val descuento: Int,
+    val beneficios: List<String>,
+    val fechaRegistro: String
+)
+
