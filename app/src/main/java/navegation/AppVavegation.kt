af606dc3240730c75a08217ecc3801ba2280b40b
Maria0412 <kothagatica@gmail.com>
2025-11-04 14:59:27 -0300

    Update AppVavegation.kt
    Se modifica el archivo navegacion, para enlazar las tres pantallas con las que trabajaremos

--- app/src/main/java/navegation/AppVavegation.kt
+++ app/src/main/java/navegation/AppVavegation.kt
@@ -1,2 +1,22 @@
 package navegation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.pasteleria.screens.*
+
+@Composable
+fun AppNavigation() {
+    val navController: NavHostController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "inicio"
+    ) {
+        composable("inicio") { InicioScreen(navController) }
+        composable("detalle") { DetalleScreen(navController) }
+        composable("registro") { RegistroScreen(navController) }
+    }
+}
