9361120fca309a6de76494f4ffbe3213ecbe6fdf
Maria0412 <kothagatica@gmail.com>
2025-11-04 14:24:15 -0300

    Update TextStyles.kt
    Se define, el estilo para títulos y párrafos que se usaran a lo largo del proyecto en el archivo TextStyles.kt

--- app/src/main/java/com/example/pasteleria/components/TextStyles.kt
+++ app/src/main/java/com/example/pasteleria/components/TextStyles.kt
@@ -1,2 +1,36 @@
 package com.example.pasteleria.components
 
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Shadow
+
+val ChocolateBrown = Color(0xFF4B2E2E)
+val ShadowBrown = Color(0xFF3D2626)
+
+val h1Style = TextStyle(
+    fontFamily = FontFamily.SansSerif,
+    fontSize = 28.sp,
+    fontWeight = FontWeight.Bold,
+    color = ChocolateBrown,
+    shadow = Shadow(
+        color = ShadowBrown,
+        offset = Offset(2f, 2f),
+        blurRadius = 3f
+    ),
+    letterSpacing = 2.sp,
+    textAlign = TextAlign.Center
+)
+
+val pStyle = TextStyle(
+    fontFamily = FontFamily.SansSerif,
+    fontSize = 16.sp,
+    color = ChocolateBrown,
+    lineHeight = 22.sp,
+    textAlign = TextAlign.Justify
+)
